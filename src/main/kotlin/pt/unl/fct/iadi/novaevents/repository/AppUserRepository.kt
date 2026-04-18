8ff3f639dffc45d719e1f929496ce83560c43b13
joaoLima17 <jml.lima@campus.fct.unl.pt>
2026-04-18 17:30:10 +0100

    first

--- src/main/kotlin/pt/unl/fct/iadi/novaevents/repository/AppUserRepository.kt
+++ src/main/kotlin/pt/unl/fct/iadi/novaevents/repository/AppUserRepository.kt
@@ -1,9 +1,11 @@
 package pt.unl.fct.iadi.novaevents.repository
 
+import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 import pt.unl.fct.iadi.novaevents.model.appUser
 @Repository
-interface AppUserRepository {
+
+interface AppUserRepository: JpaRepository<appUser, Long> {
     fun findByUsername(username: String): appUser
 
 }
